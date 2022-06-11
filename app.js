8986d8f414c9b8d08462f3a2125cd3fdd792ae86
Shixue Mai <shixuemai@outlook.com>
2022-06-11 18:36:25 -0400

    add read more on home page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash=require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -21,14 +22,19 @@ app.get("/", (req, res) => {
   res.render("home",{StartingContent:homeStartingContent, posts:posts});
 });
 
+app.get("/test/:word", function (req, res) {
+  console.log(lodash.lowerCase(req.params.word));
+}
+)
+
 app.get("/posts/:postName", (req, res) => {
+  const requestedTitle=lodash.lowerCase(req.params.postName);
+
   for(i=0; i<posts.length; i++) {
-    if(posts[i].title===req.params.postName){
-      console.log("match found")
-    };
+    if(lodash.lowerCase(posts[i].title) === requestedTitle) {
+      res.render('post', {Title: posts[i].title, Content: posts[i].content});
   }
-  
-});
+  }});
 
 app.get("/about", (req, res) => {
   res.render("about",{aboutContent:aboutContent});
